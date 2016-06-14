dccb9d38c2bd9963ec86cae746b0a59ee0952718
Jessica Paczuski <jz.paczuski@gmail.com>
2016-06-14 12:22:40 +0200

    Bolds the word restaurant

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -2,17 +2,26 @@ import React, { PropTypes as T } from 'react'
 import styles from './styles.module.css'
 
 export class Item extends React.Component {
+
   render() {
-    console.log(this.props.restaurant.photos[0].originalURL)
+
     return (
       <li key={this.props.restaurant.id} className={styles.container}>
         <img src={this.props.restaurant.photos[0].s3URL} className={styles.photo} />
         <div className={styles.textbox}>
-          <h3>{this.props.restaurant.name}</h3>
-          <h5>{this.props.restaurant.cuisines.map(cuisine => {
+          <p className={styles.title}>{this.props.restaurant.name.split(/\b/).map(word => {
+                if( word=="Restaurant"){
+                  return <strong>{word}</strong>
+                }
+                else{
+                  return word
+                }
+              })}
+          </p>
+          <p>{this.props.restaurant.cuisines.map(cuisine => {
             return cuisine
-          })}</h5>
-        </div>  
+          })}</p>
+        </div>
       </li>
     )
   }
