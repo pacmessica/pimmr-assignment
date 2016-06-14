9f98ec10f0f4f8b2650f08477b49c9926bf08dd3
Jessica Paczuski <jz.paczuski@gmail.com>
2016-06-14 16:44:00 +0200

    Reorganize routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {browserHistory, Router, Route, Redirect} from 'react-router'
-import App from './containers/App/App'
-import Container from './views/Main/Container'
+import routes from './config/routes'
 
-
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="/restaurants" component={Container}/>
-    </Route>
-  </Router>
-),document.querySelector('#root'));
+ReactDOM.render((routes, document.querySelector('#root'));
