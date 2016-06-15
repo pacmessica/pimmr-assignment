0e9ac935a93423d7ade7b66e21997327537c45a5
Jessica Paczuski <jz.paczuski@gmail.com>
2016-06-15 17:23:13 +0200

    Improve Listing spec

--- src/components/Listing/Listing.spec.js
+++ src/components/Listing/Listing.spec.js
@@ -1,24 +1,35 @@
 import React from 'react'
 import { expect } from 'chai'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 
 import Listing from './Listing'
+import Item from './Item'
 import styles from './styles.module.css'
 
 describe('<Listing />', () => {
   let wrapper;
   const restaurants = [{
-    name: 'Pasta Pasta'
+    name: 'Pasta Pasta',
+    photos: [{s3URL: "pizza.jpeg"}],
+    cuisines: ["italian"]
   }, {
-    name: "Pizza Pizza"
+    name: "Pizza Pizza",
+    photos: [{s3URL: "pizza.jpeg"}],
+    cuisines: ["italian"]
   }];
 
-  beforeEach(() => {
-    wrapper = shallow(<Listing restaurants={restaurants} />)
-  });
-
   it('has an item for each restaurant in the restaurants prop', () => {
+    wrapper = shallow(<Listing restaurants={restaurants} />)
     expect(wrapper.find('Item').length)
       .to.equal(restaurants.length);
+  })
+
+  it('passes restaurant prop to <Item />', () => {
+    wrapper = mount(<Listing restaurants={restaurants} />)
+    expect(wrapper.find(Item).first())
+      .to.not.have.prop('invalid')
+    expect(wrapper.find(Item).first())
+      .to.have.prop('restaurant').deep.equal(
+      {name: 'Pasta Pasta', photos: [{s3URL: "pizza.jpeg"}], cuisines: ["italian"]})
   })
 })
