24525676e8a435d682fc91a3835fbe1650a5a9ff
Jessica Paczuski <jz.paczuski@gmail.com>
2016-06-15 10:19:36 +0200

    Refactor restaurant name method

--- src/components/Listing/Item.js
+++ src/components/Listing/Item.js
@@ -2,22 +2,23 @@ import React, { PropTypes as T } from 'react'
 import styles from './styles.module.css'
 
 export class Item extends React.Component {
-
+  getRestaurantName() {
+    return this.props.restaurant.name.split(/\b/).map(word => {
+      if ( (/restaurant/i).test(word) ) {
+        return <strong>{word}</strong>
+      }
+      else {
+        return word
+      }
+    });
+  }
   render() {
 
     return (
       <li key={this.props.restaurant.id} className={styles.item}>
         <img src={this.props.restaurant.photos[0].s3URL} className={styles.photo} />
         <div className={styles.textbox}>
-          <p className={styles.title}>{this.props.restaurant.name.split(/\b/).map(word => {
-                if( word=="Restaurant"){
-                  return <strong>{word}</strong>
-                }
-                else{
-                  return word
-                }
-              })}
-          </p>
+          <p className={styles.title}>{this.getRestaurantName()}</p>
           <p>{this.props.restaurant.cuisines.map(cuisine => {
             return cuisine + " "
           })}</p>
